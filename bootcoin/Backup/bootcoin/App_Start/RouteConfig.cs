58b2fd53bbee7aaf8339908a50ad1a5aeacebd5b
Zefany Athalaia <zefany.athalia@binus.ac.id>
2023-01-08 14:41:55 +0700

    Added master page

---
+++ bootcoin/Backup/bootcoin/App_Start/RouteConfig.cs
@@ -0,0 +1,37 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace bootcoin
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Admin",
+                url: "Admin/{action}",
+                defaults: new { controller="Admin", action = "Index" }
+            );
+
+            routes.MapRoute(
+                name: "Trainee",
+                url: "Trainee/{action}",
+                defaults: new {controller="Trainee", action = "Index"}
+            );
+
+            routes.MapRoute(
+                name: "Debug",
+                url: "Debug/{action}",
+                defaults: new { controller = "Debug", action = "Index"}
+            );
+
+            routes.MapRoute(
+                name: "Exception",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ bootcoin/Backup/bootcoin/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace bootcoin.Controllers
+{
+    public class AdminController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Teams()
+        {
+            return View("~/Views/Admin/Teams.cshtml");
+        }
+    }
+}
