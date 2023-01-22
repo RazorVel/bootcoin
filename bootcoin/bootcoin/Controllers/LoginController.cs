8fb1e3b372f3a82a6e813ae9937c5edf22daed09
Zefany Athalia <zefany.athalia@binus.ac.id>
2023-01-22 20:23:14 +0700

    Finished login and register backend

--- bootcoin/bootcoin/Controllers/LoginController.cs
+++ bootcoin/bootcoin/Controllers/LoginController.cs
@@ -6,19 +6,51 @@ using bootcoin.Models.Domain;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Identity;
+using bootcoin.Helper;
 // using System.Web;
 // using System.Web.Mvc;
 namespace bootcoin.Controllers
 {
     public class LoginController : Controller
     {
-       
-        public ActionResult Index()
-        {            
-            return View ();
+
+        private IConfiguration _config;
+        CommonHelper _helper;
+
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+            _helper = new CommonHelper(_config);
         }
+
+        [HttpGet]
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(User userRegister)
+        {
+            string UserExistsQuery = $"Select * from Users where Email='{userRegister.Email}' and Password ='{userRegister.Password}'";
+            bool userExists = _helper.UserAlreadyExists(UserExistsQuery);
+            if (userExists)
+            {
+                return RedirectToAction("Teams", "Admin");
+            }
+            else
+            {
+                TempData["AlertMessage"] = "User Is Not Registered!";
+                return View();
+            }
+           
+        }
+
         
-      
-        
+
+
+
     }
 }
