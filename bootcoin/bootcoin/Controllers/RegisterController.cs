b0edf96d7e8c8583689094182abefebe10649819
razorvel <feliks.hartanto@binus.ac.id>
2023-01-23 12:22:15 +0700

    create register backend

--- bootcoin/bootcoin/Controllers/RegisterController.cs
+++ bootcoin/bootcoin/Controllers/RegisterController.cs
@@ -4,6 +4,12 @@ using System.Linq;
 using bootcoin.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using bootcoin.Models.Domain;
+using bootcoin.Models.Register;
+using bootcoin.Data;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System.Security.Cryptography;
+using Microsoft.EntityFrameworkCore;
 // using System.Web;
 // using System.Web.Mvc;
 
@@ -11,9 +17,54 @@ namespace bootcoin.Controllers
 {
     public class RegisterController : Controller
     {
-        public ActionResult Index()
+        private readonly BootcoinDbContext bootcoinDbContext;
+
+        public RegisterController(BootcoinDbContext bootcoinDbContext)
         {
-            return View ();
+            this.bootcoinDbContext = bootcoinDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index(Object options)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(ViewModel request)
+        {
+            var newUserId = Guid.NewGuid();
+
+            var newUser = new User()
+            {
+                UserId = newUserId,
+                Name = request.Name,
+                Email = request.Email,
+                Password = request.Password,
+                Role = "Participant",
+                Balance = 0
+            };
+
+            var newProfile = new Profile()
+            {
+                ProfileId = Guid.NewGuid(),
+                UserId = newUserId
+            };
+
+            var userExists = await bootcoinDbContext.Users.FirstOrDefaultAsync(x => x.Email == request.Email);
+
+            if (userExists == null) {
+                await bootcoinDbContext.Users.AddAsync(newUser);
+                await bootcoinDbContext.Profiles.AddAsync(newProfile);
+                await bootcoinDbContext.SaveChangesAsync();
+
+                return Redirect("/Login");
+            }
+            else
+            {
+                ViewBag.Error = "A user with the same email address already exists.";
+                return View() ;
+            }
         }
     }
 }
---
+++ bootcoin/bootcoin/Models/Register/ViewModel.cs
@@ -0,0 +1,11 @@
+﻿using bootcoin.Models.Domain;
+
+namespace bootcoin.Models.Register
+{
+    public class ViewModel
+    {
+        public string? Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
