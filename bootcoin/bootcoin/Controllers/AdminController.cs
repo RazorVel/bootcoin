54590d642df25b06807e80277fba932c00d2d69f
alvon17 <73384433+alvon17@users.noreply.github.com>
2023-02-05 09:55:21 +0700

    create admin input transaction

--- bootcoin/bootcoin/Controllers/AdminController.cs
+++ bootcoin/bootcoin/Controllers/AdminController.cs
@@ -4,7 +4,11 @@ using System.Linq;
 using bootcoin.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Web;
+using Microsoft.EntityFrameworkCore;
 using bootcoin.Data;
+using bootcoin.Models.Domain;
+using bootcoin.Models.Trainee;
 // using System.Web;
 // using System.Web.Mvc;
 
@@ -59,11 +63,103 @@ namespace bootcoin.Controllers
 
             return View();
         }
-        public IActionResult Teams()
+        public async Task<IActionResult> Teams()
         {
             SessionStart();
 
-            return View("~/Views/Admin/Teams.cshtml");
+            var users = await bootcoinDbContext.Users.ToListAsync();
+            var profiles = await bootcoinDbContext.Profiles.ToListAsync();
+
+            var people = users.Join(
+                profiles,
+                users => users.UserId,
+                profiles => profiles.UserId,
+                (users, profiles) => new
+                {
+                    Name = users.Name,
+                    Role = users.Role,
+                    Balance = users.Balance,
+                    Avatar = profiles.Avatar,
+                    Department = profiles.Department,
+                    Mbti = profiles.Mbti,
+                    Zodiac = profiles.Zodiac,
+                    FavoriteFood = profiles.FavoriteFood,
+                    FunFact = profiles.FunFact
+                }
+            );
+
+            //filter by team, possible future upgrade
+            //people = people.Where(x => x.Team == userTeam);
+
+            people = people.Where(x => x.Role == "Participant");
+
+            List<TeamMemberViewModel> models = new List<TeamMemberViewModel>();
+
+            foreach (var person in people)
+            {
+                models.Add(new TeamMemberViewModel()
+                {
+                    Name = person.Name,
+                    Role = person.Role,
+                    Balance = person.Balance,
+                    Avatar = person.Avatar,
+                    Department = person.Department,
+                    Mbti = person.Mbti,
+                    Zodiac = person.Zodiac,
+                    FavoriteFood = person.FavoriteFood,
+                    FunFact = person.FunFact
+                });
+            }
+
+            return View("~/Views/Admin/Teams.cshtml", models);
         }
+
+        [HttpPost]
+        public IActionResult Input(string agentName, int amount)
+        {
+            if (string.IsNullOrEmpty(agentName) || amount <= 0)
+                return BadRequest("Invalid input");
+
+            var targetUser = bootcoinDbContext.Users.Where(u => u.Name == agentName).FirstOrDefault();
+            if (targetUser == null)
+            {
+                return BadRequest("Target user not found");
+            }
+
+            SessionStart();
+            // Add transaction header
+            var transactionHeader = new TransactionHeader
+            {
+                SourceId = Guid.Parse(ViewBag.UserId),
+                TargetId = targetUser.UserId,
+                Date = DateTime.Now,
+                Type = "Input"
+            };
+            bootcoinDbContext.TransactionHeaders.Add(transactionHeader);
+
+            // Add transaction detail
+            var transactionDetail = new TransactionDetail
+            {
+                TransactionId = transactionHeader.TransactionId,
+                Amount = amount,
+                BalanceAfter = 1000
+            };
+            bootcoinDbContext.TransactionDetails.Add(transactionDetail);
+
+            // Update target user balance
+            targetUser.Balance += amount;
+
+            try
+            {
+                bootcoinDbContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Failed to process transaction: " + ex.Message);
+            }
+
+            return RedirectToAction("Teams");
+        }
+
     }
 }
